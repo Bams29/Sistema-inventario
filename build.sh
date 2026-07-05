d544c3b9664e11ab7b66acf40c54d2bdc56e9903
Bams29 <sarmientobryan2911@gmail.com>
2026-07-05 11:40:14 -0500

    ...

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 set -e
 
 echo "Installing root dependencies..."
-npm install --legacy-peer-deps
+NODE_ENV=development npm install --legacy-peer-deps
 
 echo "Building React app..."
 cd react-app
-npm install --legacy-peer-deps
+NODE_ENV=development npm install --legacy-peer-deps
 npm run build
 cd ..
 
