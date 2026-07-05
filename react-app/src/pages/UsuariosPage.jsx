4420cbb35596f97b217353fcc2791f7913e43827
Bams29 <sarmientobryan2911@gmail.com>
2026-07-05 13:07:11 -0500

    aun buscando solucion

--- react-app/src/pages/UsuariosPage.jsx
+++ react-app/src/pages/UsuariosPage.jsx
@@ -2,11 +2,12 @@ import UsuariosTable from '../components/Usuarios/UsuariosTable'
 
 function UsuariosPage() {
   return (
-    <div className="page-container">
-      <h1>Usuarios</h1>
+    <div className="page-container usuarios-page">
+      <h1 className="usuarios-page-title">Usuarios</h1>
       <UsuariosTable />
     </div>
   )
 }
+
 
 export default UsuariosPage
