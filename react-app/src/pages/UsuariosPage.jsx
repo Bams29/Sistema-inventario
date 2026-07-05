19d2778d93d04ecef75f825e49d970627da29580
Bams29 <sarmientobryan2911@gmail.com>
2026-07-05 13:30:24 -0500

    ss

--- react-app/src/pages/UsuariosPage.jsx
+++ react-app/src/pages/UsuariosPage.jsx
@@ -3,7 +3,6 @@ import UsuariosTable from '../components/Usuarios/UsuariosTable'
 function UsuariosPage() {
   return (
     <div className="page-container usuarios-page">
-      <h1 className="usuarios-page-title">Usuarios</h1>
       <UsuariosTable />
     </div>
   )
